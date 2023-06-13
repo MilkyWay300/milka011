1bdd105e61738c825c7c9be2152625e4b6a06c49
Neto test <anyablashonkova@mail.ru>
2023-06-13 19:00:09 +0300

    Initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+
+    public String day;
+    public String year;
+    public String month;
+}
